5c2829be189837f6e2a1f162cc42a4e278d1c478
Chairul yusuf <sahabatcoding29@gmail.com>
2024-07-22 13:47:46 +0700

    add library

--- src/application/web.js
+++ src/application/web.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { errorMiddleware } from '../middleware/error-middleware.js'
 import { publicRouter } from '../router/public-api.js'
 import { userRouter } from '../router/api.js'
 
 const web = express()
+web.use(cors())
 web.use(express.json())
 web.use(publicRouter)
 web.use(userRouter)
